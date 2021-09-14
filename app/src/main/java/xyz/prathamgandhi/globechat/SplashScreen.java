f2a642a6b48604aed87f1048fa91058871d66d9a
Pratham Gandhi <ppg.1382@gmail.com>
2021-09-14 23:25:27 +0530

    Cleaned up SplashScreen

--- app/src/main/java/xyz/prathamgandhi/globechat/SplashScreen.java
+++ app/src/main/java/xyz/prathamgandhi/globechat/SplashScreen.java
@@ -10,41 +10,17 @@ import android.content.SharedPreferences.Editor;
 
 
 public class SplashScreen extends AppCompatActivity {
-    public final String PREFS_NAME = "MyPrefsFile";
     private final int SPLASH_DISPLAY_LENGTH = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        Editor editor = settings.edit();
-        Runnable go_to_namepage;
-        if(settings.getBoolean("first_launch", true)) {
-
-            go_to_namepage = new Runnable() {
-                @Override
-                public void run() {
-                    Intent nameIntent = new Intent(SplashScreen.this, NamePage.class);
-                    SplashScreen.this.startActivity(nameIntent);
-                    SplashScreen.this.finish();
-                }
-            };
-            editor.putBoolean("first_launch", false);
-            editor.apply();
-        }
-        else {
-            go_to_namepage = new Runnable() {
-                @Override
-                public void run() {
-                    Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
-                    SplashScreen.this.startActivity(mainIntent);
-                    SplashScreen.this.finish();
-                }
-
-            };
-        }
         Handler h = new Handler();
-        h.postDelayed(go_to_namepage, SPLASH_DISPLAY_LENGTH);
-    }
+        h.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, NamePage.class);
+            SplashScreen.this.startActivity(intent);
+            SplashScreen.this.finish();
+        }, SPLASH_DISPLAY_LENGTH);
 
+    }
 }
