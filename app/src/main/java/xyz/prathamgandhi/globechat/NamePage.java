029687c1b1082d463408c4121623804447c1af74
Pratham Gandhi <ppg.1382@gmail.com>
2021-09-14 16:25:08 +0530

    Name Page created, added appropriate required functioning

---
+++ app/src/main/java/xyz/prathamgandhi/globechat/NamePage.java
@@ -0,0 +1,96 @@
+package xyz.prathamgandhi.globechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+public class NamePage extends AppCompatActivity {
+    private JSONArray jarray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_page);
+        EditText personName = findViewById(R.id.personName);
+        Button submitButton = findViewById(R.id.submitName);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        URI uri = URI.create("http://192.168.29.140:5000");
+        Socket socket = IO.socket(uri);
+        if (!socket.connected()) {
+            socket.connect();
+        }
+        socket.on("connect", args -> {
+            socket.emit("askUsers");
+            socket.on("allUsers", args1 -> {
+                try {
+                jarray = ((JSONObject) args1[0]).getJSONArray("users");
+                }
+                catch (JSONException e) {
+                e.printStackTrace();
+                }
+            });
+
+        });
+        submitButton.setOnClickListener(v -> {
+            if (!socket.connected()) {
+                Toast.makeText(NamePage.this, "Please wait a second", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                socket.emit("askUsers");
+                socket.on("allUsers", args -> runOnUiThread(() -> {
+                    try {
+                        jarray = ((JSONObject) args[0]).getJSONArray("users");
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }));
+                boolean flag = false;
+                String userName = personName.getText().toString().trim();
+                if(userName.length() == 0) {
+                    Toast.makeText(NamePage.this, "Please Enter a name to continue", Toast.LENGTH_SHORT).show();
+                    flag = true;
+                }
+                else if(userName.length() > 20){
+                    Toast.makeText(NamePage.this, "Username too long!!", Toast.LENGTH_SHORT).show();
+                    flag = true;
+                }
+                else {
+                    for(int i = 0; i < jarray.length(); i++) {
+                        try {
+                            if (userName.equals(jarray.getJSONObject(i).getString("name"))) {
+                                Toast.makeText(NamePage.this, "That name has already been taken", Toast.LENGTH_SHORT).show();
+                                flag = true;
+                                break;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                if(!flag){
+                    socket.disconnect();
+                    Intent intent = new Intent(NamePage.this, MainActivity.class);
+                    NamePage.this.startActivity(intent);
+                    NamePage.this.finish();
+                }
+            }
+        });
+    }
+}
